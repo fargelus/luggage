b29a21d52415f3f27689e2389406c1947ce86c79
Dmitriy Nemykin <ddraudred@gmail.com>
2020-08-05 11:05:39 +0300

    Largest less used file

--- lib/luggage/files.rb
+++ lib/luggage/files.rb
@@ -7,6 +7,8 @@ end
 
 def largest_less_used_file(path, amount: 1)
   files = get_files_in_dir(path)
-  files.sort_by { |file| File.size("#{path}/#{file}") }
-       .min_by(amount) { |file| File.atime("#{path}/#{file}") }
+  files.min_by(amount) { |file| File.atime("#{path}/#{file}") }
+  return files.first if files.size == 1
+
+  files.max_by { |file| File.size("#{path}/#{file}") }
 end
