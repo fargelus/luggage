dcb8322d29d36f6b4b8bb15f43b1cd0a3b85d62b
Dmitriy Nemykin <ddraudred@gmail.com>
2021-01-12 10:25:03 +0300

    Sorts output

--- lib/luggage.rb
+++ lib/luggage.rb
@@ -3,6 +3,7 @@
 require_relative 'luggage/version'
 require_relative 'luggage/helpers'
 require_relative 'luggage/options'
+require_relative 'luggage/reporter'
 
 module Luggage
   class Error < StandardError; end
@@ -15,21 +16,12 @@ module Luggage
       @options = parse_options
       @dir_path = ARGV[0]
       @count = ARGV[1].to_i || 1
+      @reporter = Reporter.new(@dir_path, @options)
     end
 
     def print_report
-      luggage = collect_luggage
-      exit(0) if luggage.empty?
-
-      spaces = luggage.max_by { |file, *| file.size }.first.size + 5
-      puts 'Size'.ljust(spaces) + 'File'.ljust(spaces) + 'Used'
-
-      luggage.each do |file, timing|
-        full_path = "#{@dir_path}/#{file}"
-        file_size, measure = get_pretty_file_size(full_path)
-        time_without_zone = timing.strftime('%d %b %Y %H:%M:%S')
-        puts "#{file_size} #{measure}".ljust(spaces) + file.ljust(spaces) + time_without_zone
-      end
+      @reporter.files = collect_luggage
+      @reporter.print
     end
 
     private
---
+++ lib/luggage/reporter.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require_relative 'helpers'
+
+module Luggage
+  class Reporter
+    include Helpers
+
+    attr_reader :spaces; private :spaces
+
+    def initialize(dir, options)
+      @dir = dir
+      @options = options
+    end
+
+    attr_writer :files
+
+    def print
+      exit(0) if @files.empty?
+
+      @spaces = @files.max_by { |file, *| file.size }.first.size + 5
+      header
+      return default_print if @options[:sort].nil? || @options[:sort] == 'size'
+
+      sorted_by_time_print
+    end
+
+    private
+
+    def header
+      puts 'Size'.ljust(spaces) + 'File'.ljust(spaces) + 'Used'
+    end
+
+    def default_print
+      @files.each do |file, timing|
+        print_row(file, timing)
+      end
+    end
+
+    def print_row(file, timing)
+      full_path = "#{@dir}/#{file}"
+      file_size, measure = get_pretty_file_size(full_path)
+      time_without_zone = timing.strftime('%d %b %Y %H:%M:%S')
+      puts "#{file_size} #{measure}".ljust(spaces) + file.ljust(spaces) + time_without_zone
+    end
+
+    def sorted_by_time_print
+      @files.sort_by { |*, timing| timing }.each do |file, timing|
+        print_row(file, timing)
+      end
+    end
+  end
+end
